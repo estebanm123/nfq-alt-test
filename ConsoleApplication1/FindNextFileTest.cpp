67c916c540eb1738e967c5b739ab19f169635977
Esteban Margaron <emargaron@microsoft.com>
2023-01-20 10:20:38 -0800

    Update findNextFile test to store metadata + add extra loop test

--- ConsoleApplication1/FindNextFileTest.cpp
+++ ConsoleApplication1/FindNextFileTest.cpp
@@ -23,15 +23,13 @@ long toMs(std::chrono::nanoseconds t)
 
 void executeFindNextFileTest()
 {
+	// Replace this with a test folder
 	LPCWSTR parentPath = L"E:\\bigFolders\\20k\\*";
-
-	auto parentPathHstring = winrt::hstring(parentPath);
 
 	WIN32_FIND_DATAW findData;
 
-
 	auto preFindFirstFileT = Clock::now();
-	// TODO: would we ever need to support ANSI alternative? - in/out types are different
+	// TODO: would we ever need to support ANSI alternative? - in/out types are different.
 	HANDLE handle = FindFirstFile(parentPath, &findData);
 	if (handle == INVALID_HANDLE_VALUE || handle == NULL)
 	{
@@ -41,19 +39,26 @@ void executeFindNextFileTest()
 
 	bool hasNext = false;
 
+	std::wstring dummyFileName = L"h329i0abmlw12!I(O)#";
+
 	auto preloopT = Clock::now();
-	// dummy vector - in reality store metadata to enable sorting
-	std::vector<winrt::hstring> files;
+	std::vector<WIN32_FIND_DATAW> files;
 	do
 	{
-		// TODO: store metadata
+		// TODO: can maybe optimize by avoid creating a new std::wstring and just working with the wchar* name
 		auto name = findData.cFileName;
 		auto tmp = std::wstring(name);
 		auto pos = tmp.find_last_of(L".");
-		auto ext = tmp.substr(pos);
+		auto ext = tmp.substr(pos); 
+		if (dummyFileName == tmp)
+		{
+			// This test may not be an effective simulation due to branch prediction maybe skipping it
+			printf("This conditional block is just to simulate a check we may have to do");
+		}
+
 		if (supportedExtensions.find(ext) != supportedExtensions.end())
 		{
-			files.push_back(name);
+			files.push_back(findData);
 		}
 		hasNext = FindNextFile(handle, &findData);
 	} while (hasNext);
@@ -61,7 +66,17 @@ void executeFindNextFileTest()
 	printf("Loop: %lu\n", toMs(doneT - preloopT));
 	printf("Loop w/ initial find: %lu\n", toMs(doneT - preFindFirstFileT));
 	printf("num files: %u\n", files.size());
-
+	
+	auto preloop2T = Clock::now();
+	for (int i = 0; i < files.size(); i++)
+	{
+		if (dummyFileName == files[i].cFileName)
+		{
+			printf("This conditional block is just to simulate a check we may have to do");
+		}
+	}
+	auto loop2DoneT = Clock::now();
+	printf("Loop 2: %lu\n", toMs(loop2DoneT - preloop2T));
 
 	auto lastError = GetLastError();
 	if (lastError == ERROR_NO_MORE_FILES)
