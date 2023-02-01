c5191831dadafaa526f0580eaaeeb311162da637
Samuel Chapleau <sachaple@microsoft.com>
2023-01-31 16:11:42 -0800

    Add perf test and listview of sort order

--- NfqApp/MainWindow.xaml.cs
+++ NfqApp/MainWindow.xaml.cs
@@ -1,22 +1,85 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using NfqLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using WinRT.Interop;
+
+#nullable enable
 
 namespace NfqApp
 {
+    [INotifyPropertyChanged]
     public sealed partial class MainWindow : Window
     {
+        [ObservableProperty]
+        IStorageItem? activatedItem;
+
+        [ObservableProperty]
+        IList<FileExplorerSort> fileExplorerSortOrder = new List<FileExplorerSort>();
+
+        [ObservableProperty]
+        TimeSpan timeToRetrieveSort;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        public static Visibility HasSortOrder(IList<FileExplorerSort> fileExplorerSortOrder) => fileExplorerSortOrder.Count > 0 ? Visibility.Visible : Visibility.Collapsed;
+
+        public static string AscendingToString(bool isAscending) => isAscending ? "Ascending" : "Descending";
+
+        public static string TimeToRetrieveSortToString(TimeSpan time) => $"Time to retrieve sort order : {time.TotalMilliseconds} (ms)";
+
+        public async Task InitializeAsync(IStorageItem? item)
         {
-            myButton.Content = "Clicked";
-            var sortColumns = FileExplorerHelper.GetSortColumns("C:\\Users\\sachaple\\Pictures\\Unsplash");
+            ActivatedItem = item;
+
+            if (ActivatedItem is IStorageItem2 storageItem2)
+            {
+                var parentFolder = await storageItem2.GetParentAsync();
+                DisplaySortColumns(parentFolder);
+            }
+        }
+
+        private async void OpenFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFolder folder = await PickFolderAsync();
+            if (folder != null)
+            {
+                DisplaySortColumns(folder);
+            }
+        }
+
+        private void DisplaySortColumns(StorageFolder? folder)
+        {
+            if (folder != null)
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                FileExplorerSortOrder = FileExplorerHelper.GetSortColumns(folder.Path);
+                stopwatch.Stop();
+                TimeToRetrieveSort = stopwatch.Elapsed;
+            }
+        }
+
+        private async Task<StorageFolder> PickFolderAsync()
+        {
+            var window = new Window();
+            var hwnd = WindowNative.GetWindowHandle(window);
+            var folderPicker = new FolderPicker();
+            folderPicker.FileTypeFilter.Add("*");
+            InitializeWithWindow.Initialize(folderPicker, hwnd);
+            return await folderPicker.PickSingleFolderAsync();
         }
     }
 }
