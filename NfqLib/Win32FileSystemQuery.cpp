2c61ec81e58b0d5c91f71196404478248ce18425
Samuel Chapleau <sachaple@microsoft.com>
2023-02-03 11:56:35 -0800

    Add placeholder file system query

---
+++ NfqLib/Win32FileSystemQuery.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "Win32FileSystemQuery.h"
+#if __has_include("Win32FileSystemQuery.g.cpp")
+#include "Win32FileSystemQuery.g.cpp"
+#endif
+
+namespace winrt::NfqLib::implementation
+{
+    Win32FileSystemQuery::Win32FileSystemQuery(const Win32FileSystemQueryOptions& queryOptions): m_queryOptions(queryOptions)
+    {
+    }
+
+    Win32FileSystemQueryOptions Win32FileSystemQuery::QueryOptions()
+    {
+        return m_queryOptions;
+    }
+
+    Windows::Foundation::IAsyncOperation<Windows::Foundation::Collections::IVector<winrt::hstring>> Win32FileSystemQuery::EnumerateAsync()
+    {
+        co_return single_threaded_vector<winrt::hstring>();
+    }
+}
---
+++ NfqLib/Win32FileSystemQuery.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "Win32FileSystemQuery.g.h"
+
+namespace winrt::NfqLib::implementation
+{
+    struct Win32FileSystemQuery : Win32FileSystemQueryT<Win32FileSystemQuery>
+    {
+        Win32FileSystemQuery(const Win32FileSystemQueryOptions& queryOptions);
+
+        winrt::event_token FileSystemChanged(FileSystemChangedEventHandler const& handler) { return m_fileSystemChangede.add(handler); }
+        void FileSystemChanged(winrt::event_token const& token) { m_fileSystemChangede.remove(token); }
+
+        Win32FileSystemQueryOptions QueryOptions();
+
+        Windows::Foundation::IAsyncOperation<Windows::Foundation::Collections::IVector<winrt::hstring>> EnumerateAsync();
+
+    private:
+        Win32FileSystemQueryOptions m_queryOptions;
+        winrt::event<FileSystemChangedEventHandler> m_fileSystemChangede;
+    };
+}
+
+namespace winrt::NfqLib::factory_implementation
+{
+    struct Win32FileSystemQuery : Win32FileSystemQueryT<Win32FileSystemQuery, implementation::Win32FileSystemQuery>
+    {
+    };
+}
