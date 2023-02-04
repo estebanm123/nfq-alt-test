455e0c8efc31af0e05cf0eb94345e6029c364b76
Samuel Chapleau <sachaple@microsoft.com>
2023-02-03 17:57:40 -0800

    Working win32 file enum returning just file name

---
+++ NfqLib/Win32File.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "Win32File.h"
+#if __has_include("Win32File.g.cpp")
+#include "Win32File.g.cpp"
+#endif
+
+namespace winrt::NfqLib::implementation
+{
+    int32_t Win32File::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void Win32File::MyProperty(int32_t /*value*/)
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ NfqLib/Win32File.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "Win32File.g.h"
+
+namespace winrt::NfqLib::implementation
+{
+    struct Win32File : Win32FileT<Win32File>
+    {
+        Win32File() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::NfqLib::factory_implementation
+{
+    struct Win32File : Win32FileT<Win32File, implementation::Win32File>
+    {
+    };
+}
